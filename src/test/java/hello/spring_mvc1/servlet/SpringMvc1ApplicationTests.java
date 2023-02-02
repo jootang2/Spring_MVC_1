ab0c446b3b7087e73799b1cc31d8535f6b6e1b84
joohwan9607 <joohwan9607@mobility42.io>
2023-02-03 01:17:57 +0900

    init setting

---
+++ src/test/java/hello/spring_mvc1/servlet/SpringMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spring_mvc1.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
