ef0f0fae558b52ddbbd8f5afbc6fbc3b802a75be
joohwan9607 <joohwan9607@mobility42.io>
2023-02-03 01:38:42 +0900

    create hello servlet

--- src/main/java/hello/spring_mvc1/servlet/SpringMvc1Application.java
+++ src/main/java/hello/spring_mvc1/servlet/SpringMvc1Application.java
@@ -2,8 +2,9 @@ package hello.spring_mvc1.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@
+@ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
 public class SpringMvc1Application {
 
