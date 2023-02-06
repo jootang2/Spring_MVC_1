4bdea43012070882ff8b12ed0d00fb764c530ad9
joohwan9607 <joohwan9607@mobility42.io>
2023-02-07 00:49:51 +0900

    API 메세지 바디 - JSON

---
+++ src/main/java/hello/spring_mvc1/servlet/basic/HelloData.java
@@ -0,0 +1,10 @@
+package hello.spring_mvc1.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+    private String username;
+    private int age;
+}
