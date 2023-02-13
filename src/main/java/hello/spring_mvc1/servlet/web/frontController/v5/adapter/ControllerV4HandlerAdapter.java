38de9c8394a2ca807490806e2d9b71061f258dac
joohwan <joohwan9607@mobility42.io>
2023-02-13 19:50:16 +0900

    유연한 컨트롤러1(ControllerV4, V3 중 원하는 Controller 사용 가능) v5

---
+++ src/main/java/hello/spring_mvc1/servlet/web/frontController/v5/adapter/ControllerV4HandlerAdapter.java
@@ -0,0 +1,41 @@
+package hello.spring_mvc1.servlet.web.frontController.v5.adapter;
+
+import hello.spring_mvc1.servlet.web.frontController.ModelView;
+import hello.spring_mvc1.servlet.web.frontController.v4.ControllerV4;
+import hello.spring_mvc1.servlet.web.frontController.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV4);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws IOException, ServletException {
+        ControllerV4 controller = (ControllerV4) handler;
+
+        Map<String, String> paramMap = createParamMap(req);
+        Map<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
+
+        return mv;
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest req) {
+        Map<String, String> paramMap = new HashMap<>();
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, req.getParameter(paramName)));
+        return paramMap;
+    }
+}
