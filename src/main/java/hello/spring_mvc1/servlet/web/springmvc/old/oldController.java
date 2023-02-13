9f7a6ed310b9718714f0dae67d1af629a8e9246b
joohwan9607 <joohwan9607@mobility42.io>
2023-02-14 01:32:33 +0900

    Spring의 예전 뷰 리졸버

--- src/main/java/hello/spring_mvc1/servlet/web/springmvc/old/oldController.java
+++ src/main/java/hello/spring_mvc1/servlet/web/springmvc/old/oldController.java
@@ -11,6 +11,6 @@ public class oldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("oldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
